7769a2e7edefc7f453678b98e3fc64aaa20beef9
Ondrej Rohon <ondrej.rohon@gmail.com>
2014-02-06 19:00:56 +0100

    Adding bootstrap, fixing slide out menu height.

--- src/js/script.js
+++ src/js/script.js
@@ -4,18 +4,33 @@ var o = {};
 o.slideoutMenu = function () {
 
 	o.$body = $("body");
+	o.$window = $(window);
+	o.$slideoutMenu = $("#slideout-menu");
 
 	$(".main-nav .menu-icon").click(function(e){
 		e.preventDefault();
 		o.$body.toggleClass("slideout-menu-opened");
 	});
 
+	//fix height
+	o.slideoutMenuHeight();
+
+}
+o.slideoutMenuHeight = function () {
+	o.$slideoutMenu.height( o.$window.height() );
 }
 // END slideout menu
 
-
+// DOM ready
 $(function(){
 
 	o.slideoutMenu();
 
 });
+// END DOM ready
+
+// Window resize
+$(window).resize(function(){
+	o.slideoutMenuHeight();
+});
+// END window resize
