9dbef5703b8adbac8c236d4085e40265677801bc
Ondrej Rohon <ondrej.rohon@gmail.com>
2014-02-06 17:25:00 +0100

    Adding slide out menu for mobile version of site.

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,21 @@
 var o = {};
 
+// Slideout menu
+o.slideoutMenu = function () {
+
+	o.$body = $("body");
+
+	$(".main-nav .menu-icon").click(function(e){
+		e.preventDefault();
+		o.$body.toggleClass("slideout-menu-opened");
+	});
+
+}
+// END slideout menu
+
 
 $(function(){
 
-
+	o.slideoutMenu();
 
 });
