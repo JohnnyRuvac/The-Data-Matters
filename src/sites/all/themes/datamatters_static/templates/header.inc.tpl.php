248c57912e9242dd1c797c89f03eed01a3138629
Ondrej Rohon <ondrej.rohon@gmail.com>
2014-03-04 16:00:18 +0100

    Correcting main menu items.

--- src/sites/all/themes/datamatters_static/templates/header.inc.tpl.php
+++ src/sites/all/themes/datamatters_static/templates/header.inc.tpl.php
@@ -8,8 +8,8 @@
 							<a class="label active" href=""><?=$active_menu?></a>
 							<div class="filter-content">
 								<ul class="clearfix">
-									<li><a class="home <?php if($active_menu == "Interactive Map") print "active";?>" href="<?=base_path()?>">Home</a></li>
-									<li class="map"><a class="<?php if($active_menu == "Interactive Map")?>" href="<?=base_path()?>#map">Interactive Map</a></li>
+									<li><a class="home <?php if($active_menu == "Home") print "active";?>" href="<?=base_path()?>">Home</a></li>
+									<li><a class="<?php if($active_menu == "Interactive Map") print "active";?>" href="<?=base_path()?>map">Interactive Map</a></li>
 									<li><a class="<?php if($active_menu == "Projects") print "active";?>" href="<?=base_path()?>projects">Projects</a></li>
 									<li><a class="<?php if($active_menu == "Organizations") print "active";?>" href="<?=base_path()?>organizations">Organizations</a></li>
 									<li><a class="<?php if($active_menu == "About") print "active";?>" href="<?=base_path()?>about">About</a></li>
