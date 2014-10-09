2e73ea67de73f28c1ac2a56239a101a2528b3d65
Ondrej Rohon <ondrej.rohon@gmail.com>
2014-10-09 17:33:41 +0200

    Loading countries and projects in json.

--- src/sites/all/themes/datamatters_static/js/home.js
+++ src/sites/all/themes/datamatters_static/js/home.js
@@ -6,6 +6,44 @@ o.setHeight = function() {
 
 }
 
+o.loadCountriesAndProjects = function() {
+  
+  var bothComplete = 0; //bothComplete has to be == 2, it means that we have loaded both jsons
+  var url = $('#map-container').attr('data-json-url');
+
+  $.ajax({
+    'async': false,
+    'global': false,
+    'url': url + "json/countries",
+    'dataType': "json",
+    'success': function (data) {
+      o.countriesJson = data;
+      //o.map.highlightCountriesWithProject();
+      bothComplete++;
+      if (bothComplete == 2) {
+      	//o.countries.initHoverAndClick();
+      	//o.countries.center();
+      }
+    }
+  });
+
+  $.ajax({
+    'async': false,
+    'global': false,
+    'url': url + "json/projects",
+    'dataType': "json",
+    'success': function (data) {
+      o.projectsJson = data;
+      bothComplete++;
+      if (bothComplete == 2) {
+      	//o.countries.initHoverAndClick();
+      	//o.countries.center();
+      }
+    }
+  });
+
+}
+
 $(function(){
 
 	o.$mainContent = $(".main-content");
@@ -13,7 +51,9 @@ $(function(){
 
 	L.mapbox.accessToken = 'pk.eyJ1Ijoib25kcmVqcm9ob24iLCJhIjoiVFVwWWtPOCJ9.tqWegHdwlqVYKPD-d2IFhQ';
 	var map = L.mapbox.map('map-container', 'ondrejrohon.oeyfzuxr')
-  					 .setView([40, -74.50], 9);
+  					 .setView([40, -74.50], 3);
+
+  o.loadCountriesAndProjects();
 
 });
 
