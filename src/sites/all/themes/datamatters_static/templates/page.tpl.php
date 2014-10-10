613df4efb3539c3617c196f3674e3890e0fdf2e8
Ondrej Rohon <ondrej.rohon@gmail.com>
2014-10-10 19:40:27 +0200

    Fixing error after merge.

---
+++ src/sites/all/themes/datamatters_static/templates/page.tpl.php
@@ -0,0 +1,24 @@
+<div id="outer-wrapper" class="clearfix">
+    <div id="wrapper">
+      <? include("header.inc.tpl.php"); ?>
+      <div class="main-content">
+        <?php print render($page['content']);?>
+      </div>
+
+    </div>
+    
+  <?php include("slideout-menu.inc.tpl.php"); ?>
+    
+    
+  </div>
+
+
+    <?php if ($primary_local_tasks && $title != "User account"): ?><ul class='links clearfix action-links'><?php print render($primary_local_tasks) ?></ul><?php endif; ?>
+    <?php if ($secondary_local_tasks): ?><ul class='links clearfix'><?php print render($secondary_local_tasks) ?></ul><?php endif; ?>
+
+<?php if ($page['help'] || ($show_messages && $messages)): ?>
+  <div id='console'><div class='limiter clearfix'>
+    <?php print render($page['help']); ?>
+    <?php if ($show_messages && $messages): print $messages; endif; ?>
+  </div></div>
+<?php endif; ?>
